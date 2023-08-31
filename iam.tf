1cfbdb00082603a51deb16e118e18a9f88f7a639
Canux <canuxcheng@gmail.com>
2023-08-31 16:16:08 +0800

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -9,18 +9,9 @@ resource "vault_auth_backend" "kubernetes" {
   path = "${local.project_prefix}${var.cluster_name}"
 }
 
-# https://developer.hashicorp.com/vault/docs/auth/kubernetes#use-the-vault-client-s-jwt-as-the-reviewer-jwt
-# Use the Vault client's JWT as the reviewer JWT
-#
-# When configuring Kubernetes auth, you can omit the
-# token_reviewer_jwt, and Vault will use the Vault
-# client's JWT as its own auth token when communicating
-# with the Kubernetes TokenReview API. If Vault is
-# running in Kubernetes, you also need to set disable_local_ca_jwt=true.
-
 resource "vault_kubernetes_auth_backend_config" "default" {
   backend                = vault_auth_backend.kubernetes.path
-  kubernetes_host        = data.google_container_cluster.default.endpoint
+  kubernetes_host        = "https://${data.google_container_cluster.default.endpoint}"
   kubernetes_ca_cert     = base64decode(data.google_container_cluster.default.master_auth[0].cluster_ca_certificate)
   disable_iss_validation = true
   disable_local_ca_jwt = true
